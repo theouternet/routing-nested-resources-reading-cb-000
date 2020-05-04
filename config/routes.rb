7f36ae9fabfff44cd663352c8c14a5d9e34723d7
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-04 01:08:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show]
 end 
 
-resources :posts, only: [:show, :index]
-
+ resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+ 
   root 'posts#index'
 
 end
